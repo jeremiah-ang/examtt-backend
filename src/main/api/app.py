b88892a0b69a629b14af0164516e6c96c19fbb4a
jeremiah-ang <jeremiahang94@gmail.com>
2018-11-14 14:52:24 +0800

    Add 404

--- src/main/api/app.py
+++ src/main/api/app.py
@@ -4,6 +4,14 @@ from controller import Controller
 app = Flask(__name__)
 controller = Controller()
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
     return controller.help()
+
+@app.errorhandler(404)
+def page_not_found():
+    return controller.page_not_found()
+
+
+if __name__ == '__main__':
+    app.run()
--- src/main/api/controller.py
+++ src/main/api/controller.py
@@ -1,3 +1,6 @@
 class Controller:
     def help(self):
         return "Help Document"
+
+    def page_not_found(self):
+        return "Page Not Found!"
