6f150a6c58754ad2cfb282f6f34963f0f9c31a1c
jeremiah-ang <jeremiahang94@gmail.com>
2018-11-19 04:42:54 +0800

    Access control localhost

--- src/main/api/app.py
+++ src/main/api/app.py
@@ -22,7 +22,7 @@ controller = Controller(db)
 @app.after_request
 def after_request(response):
     response.headers.add(
-        'Access-Control-Allow-Origin', 'https://hopenus.github.io')
+        'Access-Control-Allow-Origin', 'http://localhost:9876')
     response.headers.add(
         'Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add(
